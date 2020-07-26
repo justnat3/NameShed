ce789d052eee87342a077d15e3d2dd2115b8f502
Nate <58314490+justnat3@users.noreply.github.com>
2020-07-25 19:18:28 -0500

    Update apiTest.py

--- apiTest.py
+++ apiTest.py
@@ -2,12 +2,13 @@ import api
 import requests
 from pyassert import assert_that
 
+# Is Thread Alive
 def threadAlive():
     resp = requests.get('http://127.0.0.1:5001')
     if resp.ok == True: print('PASS')
     else: print('FAIL')
     assert_that(resp.ok).is_true()
     
-
+# Driver Code
 if __name__ == "__main__":
     threadAlive()
