cd58a77f0207dbafdb56cd2e823f01ba902f8978
justnat3 <nd2010@reedusa.com>
2020-08-01 08:34:21 -0500

    Vercel compatib

--- api/api.py
+++ api/api.py
@@ -4,7 +4,7 @@ import string
 from flask import Flask, got_request_exception, jsonify, request
 from flask_restful import Api, Resource, reqparse
 
-from generator import Drive, addPrefix
+from util.generator import Drive, addPrefix
 
 app = Flask(__name__)
 api = Api(app)
